9027193f01ddf72570885952b3f6f5a1977771e4
Dulal-Ahmed <dulalaahmedjalal@gmail.com>
2023-01-23 21:45:49 +0600

    islamicwebsite

---
+++ app.js
@@ -0,0 +1,28 @@
+// Select 
+// Select 
+// Select 
+
+let Logo = document.querySelector('.logo');
+let Bukari = document.querySelector('.bukari');
+let Muslim = document.querySelector('.muslim');
+let Majah = document.querySelector('.majah');
+let Dawd = document.querySelector('.dawd');
+
+Logo.addEventListener("click", () =>{
+    location.href = 'index.html';
+})
+Bukari.addEventListener("click", () =>{
+    location.href = 'bukari.html';
+})
+
+Muslim.addEventListener("click", () =>{
+    location.href = 'muslim.html';
+})
+
+Majah.addEventListener("click", () =>{
+    location.href = 'majah.html';
+})
+
+Dawd.addEventListener("click", () =>{
+    location.href = 'dawd.html';
+})
